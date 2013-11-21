ce36135fa2329ffc938789bd25fa559c8a9e1dc5
Nathan Kot <me@nathankot.com>
2013-11-21 15:05:35 +1300

    Fix up register controller on invalid emails.

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -4,12 +4,16 @@ angular.module('meetingroomApp')
   .controller('RegisterCtrl', function ($scope, $http, API_ROOT) {
 
     $scope.state = {};
-    $scope.save = function(model) {
-      $http.post(API_ROOT + '/users', { user: model }).success(function() {
-        $scope.state.registered = true;
-      }).error(function(res) {
-        $scope.state.errors = res;
-      });
+    $scope.save = function(user) {
+      if (user.$valid) {
+        $http.post(API_ROOT + '/users', { user: user }).success(function() {
+          $scope.state.registered = true;
+        }).error(function(res) {
+          $scope.state.errors = res;
+        });
+      } else {
+        $scope.state.errors = ['Email is invalid'];
+      }
     };
 
   });
--- test/spec/controllers/register.js
+++ test/spec/controllers/register.js
@@ -8,12 +8,14 @@ describe('Controller: RegisterCtrl', function () {
   var RegisterCtrl,
     scope,
     $httpBackend,
+    $http,
     API_ROOT;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope, $injector) {
     scope = $rootScope.$new();
     $httpBackend = $injector.get('$httpBackend');
+    $http = $injector.get('$http');
     API_ROOT = $injector.get('API_ROOT');
     RegisterCtrl = $controller('RegisterCtrl', { $scope: scope });
   }));
@@ -25,42 +27,61 @@ describe('Controller: RegisterCtrl', function () {
 
     describe('clicks on submit and save is called', function() {
       beforeEach(function() {
-        spyOn(scope, 'save').andCallThrough();
+        spyOn($http, 'post').andCallThrough();
       });
 
-      it('should make a post request.', function() {
-        $httpBackend.expectPOST(API_ROOT + '/users').respond(201, { email: 'me@nathankot.com' });
-        scope.save(scope.user);
-        $httpBackend.flush();
-      });
-
-      describe('email does not already exist', function() {
+      describe('email is invalid', function() {
         beforeEach(function() {
-          $httpBackend.when('POST', API_ROOT + '/users').respond(201, { email: 'me@nathankot.com' });
+          scope.user.$invalid = true;
+          scope.user.$valid = false;
+        });
+
+        it('should not make the request', function() {
+          scope.save(scope.user);
+          expect($http.post).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('email is valid', function() {
+        beforeEach(function() {
+          scope.user.$valid = true;
+        });
+
+        it('should make a post request.', function() {
+          $httpBackend.expectPOST(API_ROOT + '/users').respond(201, { email: 'me@nathankot.com' });
           scope.save(scope.user);
           $httpBackend.flush();
         });
 
-        it('should set registered', function() {
-          expect(scope.state.registered).toBe(true);
+        describe('email does not already exist', function() {
+          beforeEach(function() {
+            $httpBackend.when('POST', API_ROOT + '/users').respond(201, { email: 'me@nathankot.com' });
+            scope.save(scope.user);
+            $httpBackend.flush();
+          });
+
+          it('should set registered', function() {
+            expect(scope.state.registered).toBe(true);
+          });
+        });
+
+        describe('email already exists', function() {
+          beforeEach(function() {
+            $httpBackend.when('POST', API_ROOT + '/users').respond(412, ['Email already exists']);
+            scope.save(scope.user);
+            $httpBackend.flush();
+          });
+
+          it('should set the errors', function() {
+            expect(scope.state.errors.length).toEqual(1);
+          });
+
+          it('shouldnt set registered', function() {
+            expect(scope.state.registered).not.toBe(true);
+          });
         });
       });
 
-      describe('email already exists', function() {
-        beforeEach(function() {
-          $httpBackend.when('POST', API_ROOT + '/users').respond(412, ['Email already exists']);
-          scope.save(scope.user);
-          $httpBackend.flush();
-        });
-
-        it('should set the errors', function() {
-          expect(scope.state.errors.length).toEqual(1);
-        });
-
-        it('shouldnt set registered', function() {
-          expect(scope.state.registered).not.toBe(true);
-        });
-      });
     });
   });
 });
