d161569b0d90c84d21a940ea3a73530ad2422321
Nathan Kot <me@nathankot.com>
2013-11-21 15:53:48 +1300

    Better way of doing it.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,13 +3,4 @@
 angular.module('meetingroomApp')
   .controller('MainCtrl', function ($scope) {
 
-    $scope.state = {};
-
-    $scope.toggleList = function() {
-      if (typeof($scope.state.listRevealed) === 'undefined') {
-        $scope.state.listRevealed = false;
-      }
-      $scope.state.listRevealed = !$scope.state.listRevealed;
-    };
-
   });
